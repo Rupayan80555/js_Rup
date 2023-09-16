148491ca7e19f38495d2b4185bb6bea30f64d53e
Rupayan Nandi <rupayannandi8055@gmail.com>
2023-09-16 13:10:33 +0530

    about variable

---
+++ 01_bas/01_var.js
@@ -0,0 +1,13 @@
+const accountId="123589545841"
+let accountEmail="king@gmail.com"
+var accountPassword= "1234"
+accountCity ="Kolkata"
+let accountState;
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+accountEmail="Royal@gmail.com"
+accountPassword ="5678"
+accountCity="Durgapur"
+console.table([accountId,accountEmail,accountPassword,accountCity])
