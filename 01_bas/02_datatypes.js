9c9f348f2c93d2c3001cbf850f763f31e9870238
Rupayan Nandi <rupayannandi8055@gmail.com>
2023-09-16 16:07:33 +0530

    type conversion

---
+++ 01_bas/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all js code as new version
+
+null // it is a data type as well as a standalone value
+// symbol => unique
+console.log(typeof null) // output object
+console.log(typeof undefined) // output undefined
---
+++ 01_bas/03_conversion.js
@@ -0,0 +1,14 @@
+let score ="1234"
+console.log(typeof (score))
+let value = Number(score)
+console.log(typeof value)
+
+/*
+ #Type conversion into int(Number) Outputs:-
+    1) "33"=>33(as a number)
+    2)"33abc" => NaN
+    3)null =>0
+    3)undefined =>NaN
+    4) NaN => number
+
+*/
