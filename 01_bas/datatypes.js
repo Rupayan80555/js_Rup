b0d519558d4b903aeeaead1b1a69dee15e4a9444
Rupayan@8055 <rupayannandi8055@gmail.com>
2023-09-17 11:22:32 +0530

    datatype summary

---
+++ 01_bas/datatypes.js
@@ -0,0 +1,8 @@
+// Primitive
+
+// mane egulo call by value 
+// There are 7 types of Primitive data types : String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+//Reference (Non Primitive)
+
+// Array, Objective, Function
