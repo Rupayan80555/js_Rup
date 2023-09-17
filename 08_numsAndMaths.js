b2dab8745b02e7bd7f965cf7c13e7a0985121d2e
Rupayan@8055 <rupayannandi8055@gmail.com>
2023-09-18 01:18:31 +0530

    nice

---
+++ 08_numsAndMaths.js
@@ -0,0 +1,6 @@
+const number =23.86999
+console.log(number.toFixed(2))
+
+// math 
+
+console.log (Math.round((Math.random()*10)+1))
---
+++ 09_dates.js
@@ -0,0 +1,3 @@
+let myDate = new Date("02-25-2004")
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
