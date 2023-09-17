088f6eee2e066de1fc44bc79b70c05a353f2e29f
Rupayan@8055 <rupayannandi8055@gmail.com>
2023-09-18 00:39:29 +0530

    commit

---
+++ 01_bas/07_string.js
@@ -0,0 +1,8 @@
+const Name="Rupayan_Nandi_is_a_good_boy"
+const age =19
+// console.log(`Hello! My name is ${Name.toUpperCase()} and my age is ${age}`)
+
+// console.log(Name.charAt(2)) // kon index e ki ache dekha jai
+console.log(Name.indexOf('a'));
+console.log(Name.slice(1,3));
+console.log(Name.split("_",3)) //It creates aray from string First is separator Second is limit
