1d5ba449e6704bc68e4b52fa5a4b6a85c196db97
Rupayan@8055 <rupayannandi8055@gmail.com>
2023-09-17 11:34:11 +0530

    add memo

---
+++ 01_bas/06_datatypesAndMemory.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// mane egulo call by value 
+// There are 7 types of Primitive data types : String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+//Reference (Non Primitive)
+
+// Array, Objective, Function
+
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+// Stack( it is use for all primitive datatypes) ,Heap (is is use for non primitive datatypes)
+
+
+let userOne ={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email="Two@email.com"
+
+console.log(userOne)
