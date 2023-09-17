f4a252485ac44a28ffe360209e5cddbcb3c2b4ad
Rupayan@8055 <rupayannandi8055@gmail.com>
2023-09-17 11:10:38 +0530

    all About compare

---
+++ 01_bas/05_compare.js
@@ -0,0 +1,4 @@
+//   The equality check == and comparisons < > >= <=works differently
+//  like comparisons converts null to number, treating it as 0. 
+
+// === is strictly check it checks not only values as well as datatypes.
